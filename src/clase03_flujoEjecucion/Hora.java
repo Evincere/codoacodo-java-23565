e0740d001a943849eeaaa87d37b3b442ceb4c1ad
Semper Evincere <spereyra.jus@gmail.com>
2023-11-13 23:12:18 -0300

    clase03

---
+++ src/clase03_flujoEjecucion/Hora.java
@@ -0,0 +1,27 @@
+package clase03_flujoEjecucion;
+
+import java.time.LocalTime;
+
+public class Hora {
+
+	public static void main(String[] args) {
+		// Obtener la hora actual del sistema
+        LocalTime horaActual = LocalTime.now();
+
+        // Obtener la hora y los minutos
+        int hora = horaActual.getHour();
+        int minutos = horaActual.getMinute();
+
+        // Imprimir un saludo según la hora del sistema
+        if (hora < 12) {
+            System.out.println("Buen día");
+        } else if (hora < 18) {
+            System.out.println("Buenas tardes");
+        } else {
+            System.out.println("Buenas noches");
+        }
+
+        // Imprimir la hora actual
+        System.out.println("La hora actual es: " + hora + ":" + minutos);
+	}
+}
